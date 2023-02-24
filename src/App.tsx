447c3be3006ac432ca5bb0965c3812328830c4a9
joshcs.eth ᵍᵐ <mail@jcstein.dev>
2023-02-23 21:07:48 -0500

    add marketplace widget

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,12 @@ function App() {
       width="600px"
       height="600px"
       frameborder="0"
+    ></iframe>\<h1>Marketplace</h1>
+    <iframe
+    src="https://gateway.ipfscdn.io/ipfs/QmbAgC8YwY36n8H2kuvSWsRisxDZ15QZw3xGZyk9aDvcv7/marketplace.html?contract=0xf62B5551914FD6A7B3477eBA3bDAFD2e4514e9b2&chain=%7B%22name%22%3A%22Ethermint%22%2C%22chain%22%3A%22%22%2C%22rpc%22%3A%5B%22https%3A%2F%2Fd004-159-65-252-178.ngrok.io%22%5D%2C%22nativeCurrency%22%3A%7B%22symbol%22%3A%22EMT%22%2C%22name%22%3A%22EMT%22%2C%22decimals%22%3A18%7D%2C%22shortName%22%3A%22ethermint%22%2C%22chainId%22%3A42000%2C%22testnet%22%3Atrue%2C%22slug%22%3A%22ethermint%22%7D&listingId=0&theme=dark"
+    width="600px"
+    height="600px"
+    frameborder="0"
     ></iframe>
       <p className="read-the-docs">
         Sign in with your Ethereum wallet to mint your Rollkit + Celestia ERC-1155 tokens
