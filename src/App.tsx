a4d436a2611f17762470957d7514dd04a1deeb79
joshcs.eth ᵍᵐ <mail@jcstein.dev>
2023-02-23 20:58:48 -0500

    change title

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,13 @@ function App() {
 
   return (
     <div className="App">
-      <h1>Rollkit + Celestia NFT</h1>
+      <h1>Rollkit + Celestia NFTs</h1>
       <iframe src="https://gateway.ipfscdn.io/ipfs/QmbAgC8YwY36n8H2kuvSWsRisxDZ15QZw3xGZyk9aDvcv7/erc1155.html?contract=0x399f7D3a239753e82D36Aa5Ad8Ca29Eff247cB13&chain=%7B%22name%22%3A%22Ethermint%22%2C%22chain%22%3A%22%22%2C%22rpc%22%3A%5B%22https%3A%2F%2Fd004-159-65-252-178.ngrok.io%22%5D%2C%22nativeCurrency%22%3A%7B%22symbol%22%3A%22EMT%22%2C%22name%22%3A%22EMT%22%2C%22decimals%22%3A18%7D%2C%22shortName%22%3A%22ethermint%22%2C%22chainId%22%3A42000%2C%22testnet%22%3Atrue%2C%22slug%22%3A%22ethermint%22%7D&tokenId=0&theme=dark"
       width="600px"
       height="600px"
       ></iframe>
       <p className="read-the-docs">
-        Sign in with your Ethereum wallet to mint your Rollkit + Celestia ERC-1155 token.
+        Sign in with your Ethereum wallet to mint your Rollkit + Celestia ERC-1155 tokens
       </p>
     </div>
   )
