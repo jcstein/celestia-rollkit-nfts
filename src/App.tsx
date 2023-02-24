5e5bb5dc04247a21e415d2adc00d83c40ad05c47
joshcs.eth ᵍᵐ <mail@jcstein.dev>
2023-02-23 21:04:16 -0500

    add rollkit nft

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,13 @@ function App() {
       <iframe src="https://gateway.ipfscdn.io/ipfs/QmbAgC8YwY36n8H2kuvSWsRisxDZ15QZw3xGZyk9aDvcv7/erc1155.html?contract=0x399f7D3a239753e82D36Aa5Ad8Ca29Eff247cB13&chain=%7B%22name%22%3A%22Ethermint%22%2C%22chain%22%3A%22%22%2C%22rpc%22%3A%5B%22https%3A%2F%2Fd004-159-65-252-178.ngrok.io%22%5D%2C%22nativeCurrency%22%3A%7B%22symbol%22%3A%22EMT%22%2C%22name%22%3A%22EMT%22%2C%22decimals%22%3A18%7D%2C%22shortName%22%3A%22ethermint%22%2C%22chainId%22%3A42000%2C%22testnet%22%3Atrue%2C%22slug%22%3A%22ethermint%22%7D&tokenId=0&theme=dark"
       width="600px"
       height="600px"
+      frameBorder="0"
       ></iframe>
+      <iframe src="https://gateway.ipfscdn.io/ipfs/QmbAgC8YwY36n8H2kuvSWsRisxDZ15QZw3xGZyk9aDvcv7/erc1155.html?contract=0x399f7D3a239753e82D36Aa5Ad8Ca29Eff247cB13&chain=%7B%22name%22%3A%22Ethermint%22%2C%22chain%22%3A%22%22%2C%22rpc%22%3A%5B%22https%3A%2F%2Fd004-159-65-252-178.ngrok.io%22%5D%2C%22nativeCurrency%22%3A%7B%22symbol%22%3A%22EMT%22%2C%22name%22%3A%22EMT%22%2C%22decimals%22%3A18%7D%2C%22shortName%22%3A%22ethermint%22%2C%22chainId%22%3A42000%2C%22testnet%22%3Atrue%2C%22slug%22%3A%22ethermint%22%7D&tokenId=1&theme=dark"
+      width="600px"
+      height="600px"
+      frameborder="0"
+    ></iframe>
       <p className="read-the-docs">
         Sign in with your Ethereum wallet to mint your Rollkit + Celestia ERC-1155 tokens
       </p>
